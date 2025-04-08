44b7e73073632f8b0c15c02de73715c397909213
oroshi80 <oroshi@gmail.com>
2025-04-08 09:00:26 +0100

    Added ROT13 API

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 // POST /base64Encode
-app.post("/functions/base64Encode", (req, res) => {
+app.post("/functions/KelvinBase64Encode", (req, res) => {
   const { input } = req.body;
 
   if (typeof input !== "string") {
@@ -21,9 +21,9 @@ app.post("/functions/base64Encode", (req, res) => {
 });
 
 // GET /base64Encode
-app.get("/functions/base64Encode", (req, res) => {
+app.get("/functions/KelvinBase64Encode", (req, res) => {
   res.json({
-    name: "base64Encode",
+    name: "KelvinBase64Encode",
     description: "Encode anything to base64",
     input: {
       type: "string",
@@ -38,11 +38,48 @@ app.get("/functions/base64Encode", (req, res) => {
   });
 });
 
+// ROT13 encoder
+function rot13(str) {
+  return str.replace(/[a-zA-Z]/g, function (char) {
+    const base = char <= "Z" ? 65 : 97;
+    return String.fromCharCode(((char.charCodeAt(0) - base + 13) % 26) + base);
+  });
+}
+
+app.post("/functions/KelvinROT13", (req, res) => {
+  const { input } = req.body;
+
+  if (typeof input !== "string") {
+    return res.status(400).json({ error: "Input must be a string." });
+  }
+
+  const output = rot13(input);
+  res.json({ input, output });
+});
+
+app.get("/functions/KelvinROT13", (req, res) => {
+  res.json({
+    name: "KelvinROT13",
+    description:
+      "ROT13 cipher to input text (a simple letter substitution cipher that replaces a letter with the 13th letter after it in the alphabet",
+    input: {
+      type: "string",
+      description: "Input the data you'd like to ROT13 Encoder",
+      example: "Hello, world",
+    },
+    output: {
+      type: "string",
+      description: "ROT13 encoded string",
+      example: "Uryyb, jbeyq",
+    },
+  });
+});
+
 // Test page
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  logger.info("Server is running on port 3000");
+app.listen(3100, () => {
+  logger.info("Server is running on port 3100");
 });
